8fd4b98d26e9974de4402a4de60e987247ff0d07
Yiselproyecto <yiselcampo48@gmail.com>
2026-05-04 10:57:51 -0500

    1 consulta

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+use App\Http\Controllers\ConsultasController;
 
-Route::get('/', function () {
-    return view('prueba yisel');
-});
+
+Route::get('consultas',[ConsultasController::class,'consultas']);
